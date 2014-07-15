bc4eebf634efdf2250f402f2254698aa95ed9375
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-15 18:27:22 +0200

    BaseController now extracts the authorization token from the request

--- eve-api/src/main/groovy/eve/api/controllers/BaseController.groovy
+++ eve-api/src/main/groovy/eve/api/controllers/BaseController.groovy
@@ -1,13 +1,21 @@
 package eve.api.controllers
 
 import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
 import eve.api.either.Either
+import eve.api.either.Right
 
 abstract class BaseController {
 
     def renderWithResponse(HttpServletResponse response, Either either) {
         response.status = either.code
         return either.getData()
+    }
+
+    Either getTokenRightFromHeader(HttpServletRequest request) {
+        String token = request.getHeader('Authorization')
+
+        return [token: token, code: 200] as Right
     }
 
 }
