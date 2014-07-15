35ff8a7e970331a8366c6820bdf569926b0d302e
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-15 11:23:10 +0200

    TestController improved

--- eve-api/src/main/groovy/eve/api/controllers/EventController.groovy
+++ eve-api/src/main/groovy/eve/api/controllers/EventController.groovy
@@ -2,13 +2,22 @@ package eve.api.controllers
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import javax.servlet.http.HttpServletResponse
+import static eve.api.either.Either.bind
+import eve.api.either.*
 
 @RestController
 class EventController extends BaseController {
 
     @RequestMapping('/event')
-    public eventList() {
-        return 'hola eventos'
+    def eventList(HttpServletResponse response) {
+        return this.properties
+    }
+
+    @RequestMapping('/event-error')
+    def eventError(HttpServletResponse response) {
+        renderWithResponse response, ([hola:'mundo'] as Left)
     }
 
 }
