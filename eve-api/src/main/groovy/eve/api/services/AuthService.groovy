6f81136076d8f207b38a63ce4d7c9bfe791ce967
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-15 13:52:53 +0200

    AuthService improved

--- eve-api/src/main/groovy/eve/api/services/AuthService.groovy
+++ eve-api/src/main/groovy/eve/api/services/AuthService.groovy
@@ -3,6 +3,7 @@ package eve.api.services
 import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Value
 import eve.auth.StatelessAuth
+import eve.core.User
 
 @Service
 class AuthService {
@@ -15,12 +16,20 @@ class AuthService {
         this.statelessAuth = new StatelessAuth(this.secret)
     }
 
-    String generateToken(Map data) {
-        return this.statelessAuth.generateToken(data)
+    Either validateToken(Map data) {
+        Map tokenData = this.statelessAuth.validateToken(data.token)
+        if (tokenData) {
+            return (tokenData + [code: 200]) as Right
+        } else {
+            return [code: 401, error: 'Invalid token'] as Left
+        }
     }
 
-    Map validateToken(String token) {
-        return this.statelessAuth.validateToken(token)
+    Either createToken(Map data) {
+        Map tokenData = [name: data.name, id: data.id]
+        String token = this.statelessAuth.generateToken(tokenData)
+
+        return [token: token, code: 200] as Right
     }
 
 }
